bcc14aad01d73bb7e03e4b9017bd37eb56a2b8b9
mauriciorosascota <153687301+mauriciorosascota@users.noreply.github.com>
2023-12-12 21:27:19 -0700

    Delete Untitled-2.py

--- Untitled-2.py
+++
@@ -1,9 +0,0 @@
-import random
-factura1 = round(random.uniform(20, 100), 2)
-factura2 = round(random.uniform(20, 100), 2)
-factura3 = round(random.uniform(20, 100), 2)
-print(f"Factura 1: ${factura1}")
-print(f"Factura 2: ${factura2}")
-print(f"Factura 3: ${factura3}")
-
- 
